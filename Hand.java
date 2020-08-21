78b9441e9a10a53b70f1dbaa5a843a6e44be322d
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-21 19:12:49 +0900

    try catchで数字以外の入力時app終了

--- Hand.java
+++ Hand.java
@@ -1,6 +1,7 @@
 package Janken03;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Hand {
   private int hand;
@@ -8,14 +9,20 @@ public class Hand {
   public void setPlayerHand() {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("手を数字で入力してください");
-    System.out.print("(1：グー、2：チョキ、3：パー)：");
-    int playerHand = sc.nextInt();
-    System.out.println("--------じゃんけんぽん！--------");
-    if (playerHand > 0 && playerHand <= 3) {
-      this.hand = playerHand;
-    } else {
-      System.out.println("正しくない手を出しました、あなたの負けです");
+    try {
+      System.out.println("手を数字で入力してください");
+      System.out.print("(1：グー、2：チョキ、3：パー)：");
+      int playerHand = sc.nextInt();
+      System.out.println("--------じゃんけんぽん！--------");
+      if (playerHand > 0 && playerHand <= 3) {
+        this.hand = playerHand;
+      } else {
+        System.out.println("正しくない手を出しました、あなたの負けです");
+        System.exit(0);
+      }
+    } catch (InputMismatchException e){
+      System.out.println("---------------------------------");
+      System.out.println("数値以外が入力されました、ゲームを終了します");
       System.exit(0);
     }
 
